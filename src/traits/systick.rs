37d20bec99bcfae1b338f42420175fe8e56573c5
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-11-07 21:41:34 +0330

    add test for type id

--- src/traits/systick.rs
+++ src/traits/systick.rs
@@ -8,13 +8,23 @@ use crate::ToTime;
 
 use rtic_monotonics::systick::Systick;
 use rtic_monotonics::Monotonic;
-type Duration = <Systick as Monotonic>::Duration;
-type Instant = <Systick as Monotonic>::Instant;
+#[allow(dead_code)]
+type SystickDuration = <Systick as Monotonic>::Duration;
+#[allow(dead_code)]
+type SystickInstant = <Systick as Monotonic>::Instant;
 
-// use rtic_monotonics::systick::fugit;
-// const TIMER_HZ: u32 = 1_000;
-// type Instant = fugit::TimerInstantU32<TIMER_HZ>;
-// type Duration = fugit::TimerDurationU32<TIMER_HZ>;
+use rtic_monotonics::systick::fugit;
+const TIMER_HZ: u32 = 1_000;
+#[allow(dead_code)]
+type FugitInstant = fugit::TimerInstantU32<TIMER_HZ>;
+#[allow(dead_code)]
+type FugitDuration = fugit::TimerDurationU32<TIMER_HZ>;
+
+type Duration = FugitDuration;
+type Instant = FugitInstant;
+
+// type Duration = SystickDuration;
+// type Instant = SystickInstant;
 
 impl FromTime<u32> for Duration {
     fn from_ms(millis: u32) -> Self {
@@ -37,5 +47,27 @@ impl FromTime<u32> for Instant {
 impl ToTime<u32> for Instant {
     fn to_ms(&self) -> u32 {
         self.ticks()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::any::TypeId;
+
+    use super::*;
+
+    #[test]
+    fn test_type_ids() {
+        assert_eq!(
+            TypeId::of::<SystickDuration>(),
+            TypeId::of::<FugitDuration>()
+        );
+        assert_eq!(TypeId::of::<SystickInstant>(), TypeId::of::<FugitInstant>());
+
+        assert_ne!(
+            TypeId::of::<SystickDuration>(),
+            TypeId::of::<SystickInstant>()
+        );
+        assert_ne!(TypeId::of::<FugitDuration>(), TypeId::of::<FugitInstant>());
     }
 }
